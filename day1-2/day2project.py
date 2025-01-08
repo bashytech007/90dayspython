30f1823f19feb881d7150fac798f05b9d1b6e382
bashytech007 <bashiraremu474@gmail.com>
2025-01-08 23:49:58 +0100

    missed-day2

---
+++ day1-2/day2project.py
@@ -0,0 +1,9 @@
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+birth_year = 2025 - age
+
+
+print(f"Hello {name}! Nice to meet you.")
+print(f"You were born in {birth_year}.")
